7727561a17e5ffeabbb679b87bf406e3c616b632
RachelSmith500 <rachelsmith9721@gmail.com>
2020-10-11 15:47:56 -0500

    Set up equals function and finished click listeners

--- Server/public/scripts/client.js
+++ Server/public/scripts/client.js
@@ -9,6 +9,8 @@ function onReady(){
     $('#minus').on('click', subtract);
     $('#times').on('click', multiply);
     $('#divide').on('click', divide);
+    $('#clear').on('click', clear);
+    $('#equals').on('click', equals);
 }
 
 function add(){
@@ -30,4 +32,22 @@ function divide(){
 function clear(){
     $('#numberInputOne').val('');
     $('#numberInputTwo').val('');
+}
+
+function equals(){
+    
+    let mathEquation = {
+        operator: operator,
+        numberInputOne: $('#numberInputOne').val(),
+        numberInputTwo: $('#numberInputTwo').val()
+    }
+    console.log(mathEquation)
+
+    $.ajax({
+        url: '/calculator',
+        method:'POST',
+        data: total
+    }).then(function(response){
+        console.log('response from calculator:', response);
+    })
 }
