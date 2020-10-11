91ee901564dbe878ff75abc24eb6599ad9b5dac7
RachelSmith500 <rachelsmith9721@gmail.com>
2020-10-11 10:27:03 -0500

    Click listners and operators added

--- images/Server/public/scripts/client.js
+++ images/Server/public/scripts/client.js
@@ -2,5 +2,32 @@ console.log('js')
 
 $(document).ready(onReady);
 
+let operator;
+
 function onReady(){
+    $('#plus').on('click', add);
+    $('#minus').on('click', subtract);
+    $('#times').on('click', multiply);
+    $('#divide').on('click', divide);
+}
+
+function add(){
+    operator = "+";
+}
+
+function subtract(){
+    operator ="-";
+}
+
+function multiply(){
+    operator ="*";
+}
+
+function divide(){
+    operator ="/";
+}
+
+function clear(){
+    $('#numberInputOne').val('');
+    $('#numberInputTwo').val('');
 }
